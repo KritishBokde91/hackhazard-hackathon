309e2de6f7e03db0781acdcdb3ab8199a095fdca
Avisek Ray <biisal.int@gmail.com>
2025-04-24 15:25:15 +0530

    addded all api routes for questions , full CRUD wirth admin security

--- backend/src/app/api/v1/question.py
+++ backend/src/app/api/v1/question.py
@@ -1,16 +1,22 @@
 from typing import Annotated
-from fastapi import APIRouter, Query
-from sqlalchemy.exc import IntegrityError
-from src.app.core.exceptions.http_exceptions import DuplicateValueException
-from src.app.schemas.question import QuestionCreate, QuestionCreateInternal
+from fastapi import APIRouter, Path, Query
+from sqlalchemy.exc import IntegrityError, NoResultFound
+from src.app.core.exceptions.http_exceptions import DuplicateValueException, NotFoundException
+from src.app.schemas.question import QuestionCreate, QuestionCreateInternal, QuestionUpdate
 from src.app.schemas.user import UserResponse
 from src.app.crud.crud import question_crud
 from src.app.api.dependencies import super_user_dependency , db_dependency , redis_client_dependency
 
 router = APIRouter(prefix="/question" , tags=["Question"])
 
+@router.get("/{question_id}")
+async def get_question(db: db_dependency , redis : redis_client_dependency , question_id : Annotated[int , Path(description="Question id" , examples=[1 , 2])]):
+    try:
+        return await question_crud.get(db=db , redis=redis , id=question_id)
+    except NoResultFound:
+        raise NotFoundException(detail=f"Question not found with id {question_id}")
 
-@router.get("/")
+@router.get("s/")
 async def get_questions(db : db_dependency , redis : redis_client_dependency , limit : Annotated[int , Query(description="Limit to fecth data" , examples=[10 , 30])] , page : Annotated[int , Query(description="Page number" , examples=[1 , 2])]):
     return await question_crud.get_multi(db=db , redis=redis , order_by="id" ,  ascending=False,  limit=limit , page=page)
 
@@ -25,3 +31,18 @@ async def create_question(question : QuestionCreate , user : Annotated[UserRespo
     except IntegrityError:
         raise DuplicateValueException(detail="Question already exists")
     return data
+
+
+@router.patch("/{question_id}" , dependencies=[super_user_dependency])
+async def update_question(db : db_dependency , redis : redis_client_dependency , question_id : Annotated[int , Path(description="Question id" , examples=[1 , 2])] , question : QuestionUpdate):
+    try:
+        return await question_crud.update(db=db , redis=redis , obj_in=question , id=question_id)
+    except NoResultFound:
+        raise NotFoundException(detail=f"Question not found with id {question_id}")
+
+@router.delete("/{question_id}" , dependencies=[super_user_dependency])
+async def delete_question(db : db_dependency , redis : redis_client_dependency , question_id : Annotated[int , Path(description="Question id" , examples=[1 , 2])]):
+    try:
+        return await question_crud.delete(db=db , redis=redis , id=question_id)
+    except NoResultFound:
+        raise NotFoundException(detail=f"Question not found with id {question_id}")
