7b9cb7f56ae358a6f1db172942f01a44fa064751
Avisek Ray <biisal.int@gmail.com>
2025-04-28 07:09:56 +0530

    redireting every requests to vps hosted site url

--- frontend-nextjs/components/providers.tsx
+++ frontend-nextjs/components/providers.tsx
@@ -3,20 +3,23 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { useAuth } from "@/hooks/stores/use-auth";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 export function Providers({
     children,
     ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
     const [mounted, setMounted] = useState(false)
+    const router = useRouter();
     // check if user is authenticated
     const { checkAuth } = useAuth()
     useEffect(() => {
+        router.push("https://codinggeek.codeltix.com")
         async function mount() {
           await checkAuth()
           setMounted(true)
         }                
         mount()
-    }, [checkAuth])
+    }, [checkAuth  , router])
     if (mounted) {
         return <NextThemesProvider {...props}>{children}</NextThemesProvider>
     }
